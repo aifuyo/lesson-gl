0320d7e02e6b47f8fd0cd82fbb76c1c31e3c0018
DESKTOP-VK7HQMS\Ashy <radioeye@yandex.ru>
2018-06-14 22:57:33 +0300

    lesson 2

--- OpenGLkouza/Source.cpp
+++ OpenGLkouza/Source.cpp
@@ -1,16 +1,26 @@
 #include "glut.h"
 void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT); //GLbitfield mask
+	glRotatef(
+		1,//GLfloat angle, 
+		0, 0, 1 //GLfloat x, GLfloat y, GLfloat z
+	);
 	glutWireTeapot(1); //GLdouble size
-	glFlush();
+	glutSwapBuffers();
+}
+void idle(void) {
+	glutPostRedisplay();
 }
 int main(int argc, char *argv[]) {
 	glutInit(
 		&argc, //int *argcp
 		argv); //char **argv
+	glutInitDisplayMode(GL_DOUBLE); //unsigned int mode
 	glutInitWindowPosition(640,0); //int x, y
 	glutInitWindowSize(640,640);//int width, height
 	glutCreateWindow("title");//const char *title
 	glutDisplayFunc(display);//void (GLUTCALLBACK *func)(void)
+	glutIdleFunc(idle); //void (GLUTCALLBACK *func)(void)
 	glutMainLoop();
 	return 0;
 }
